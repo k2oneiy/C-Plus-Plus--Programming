46df7258ac6452279c5c1e0754119ae5af1853bd
k2oneiy <84692182+k2oneiy@users.noreply.github.com>
2021-07-10 03:14:28 -0700

    Add files via upload

---
+++ 16_assignment_and_increment_decrement_operator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+int main(int argc, char** argv) {
+	
+	int a = 10;
+	
+	a += 5;
+	
+	cout<<a<<endl;
+	
+	
+	//pre increment 
+	
+	int val = 5;
+	cout<<++val<<endl;
+	
+	//post increment 
+	int val1 = 10;
+	cout<<val1++<<endl;
+	cout<<val1<<endl;
+
+}
