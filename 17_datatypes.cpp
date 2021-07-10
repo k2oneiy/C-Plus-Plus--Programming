2a81dd3411a4ab2228c225b38211c252592716a7
k2oneiy <84692182+k2oneiy@users.noreply.github.com>
2021-07-10 03:29:50 -0700

    Add files via upload

---
+++ 17_datatypes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	int a = 1;
+	float b = 10.0;
+	double c = 20.3;
+	char d = 'S';
+	string name = "iyappan";
+	
+	cout<<a<<endl;
+	
+	cout<<b<<endl;
+	cout<<c<<endl;
+	cout<<d<<endl;
+	cout<<name<<endl;
+	
+	return 0;
+}
